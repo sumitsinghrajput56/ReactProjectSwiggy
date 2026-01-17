249f9aa09d438d4484163d53b7431e411568e673
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2026-01-17 22:31:10 +0530

    again installed parcel and react and react dom import react and react-dom/client

--- App.js
+++ App.js
@@ -1,4 +1,8 @@
- const heading = React.createElement("div",{className:"parent"},[React.createElement("div",{className:"child"},[React.createElement("h1",{className:"heading1"},"hello world!"),React.createElement("h1",{className:"heading2"},"hello world!")]),React.createElement("div",{className:"child1"},[React.createElement("h1",{className:"heading3"},"hello world!"),React.createElement("h1",{className:"heading4"},"hello world!")])]);
+import React from "react";
+import ReactDOM from "react-dom/client"; 
+ 
+ 
+ const heading = React.createElement("h1",{className:"parent"},"hello world");
 
 
         const root = ReactDOM.createRoot(document.getElementById("root"));
---
+++ dist/ReactProject.6bd02f5a.js
@@ -0,0 +1,28 @@
+const heading = React.createElement("div", {
+    className: "parent"
+}, [
+    React.createElement("div", {
+        className: "child"
+    }, [
+        React.createElement("h1", {
+            className: "heading1"
+        }, "hello world!"),
+        React.createElement("h1", {
+            className: "heading2"
+        }, "hello world!")
+    ]),
+    React.createElement("div", {
+        className: "child1"
+    }, [
+        React.createElement("h1", {
+            className: "heading3"
+        }, "hello world!"),
+        React.createElement("h1", {
+            className: "heading4"
+        }, "hello world!")
+    ])
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=ReactProject.6bd02f5a.js.map
